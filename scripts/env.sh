6186e0855ef9178c6e1868b6bdf1421905f512e5
wangzhuowei <wangzhuowei@bytedance.com>
2021-10-11 15:57:57 +0800

    chore: fix args

--- scripts/env.sh
+++ scripts/env.sh
@@ -11,8 +11,8 @@ fi
 # GO
 GOEXEC=${GOEXEC:-"go"}
 GOROOT=$GOROOT
-USER=$(whoami)
 
+USER=$(whoami)
 n=5000000
 body=(1024)
 concurrent=(100 200 400 600 800 1000)
@@ -21,7 +21,7 @@ sleep=0
 scpu=$((nprocs > 16 ? 3 : nprocs / 4 - 1)) # max is 3(4 cpus)
 nice_cmd=''
 if [ "$USER" == "root" ]; then
-    nice_cmd='nice -c -20'
+    nice_cmd='nice -n -20'
 fi
 cmd_server="${nice_cmd} taskset -c 0-$scpu"
 cmd_client="${nice_cmd} taskset -c $((scpu + 1))-$((nprocs - 1))"
