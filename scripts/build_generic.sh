609fedd9d9fa545300c5fe3d347152dec6b025d7
Marina-Sakai <marina.sakai@bytedance.com>
2023-03-31 17:08:12 +0800

    fix dir name

--- scripts/build_generic.sh
+++ scripts/build_generic.sh
@@ -27,8 +27,8 @@ $GOEXEC build -v -o $output_dir/bin/generic_map_10K_bencher $generic_dir/map/cli
 $GOEXEC build -v -o $output_dir/bin/generic_json_default_1KB_bencher $generic_dir/json/client/default/small
 $GOEXEC build -v -o $output_dir/bin/generic_json_default_5KB_bencher $generic_dir/json/client/default/medium
 $GOEXEC build -v -o $output_dir/bin/generic_json_default_10KB_bencher $generic_dir/json/client/default/large
-$GOEXEC build -v -o $output_dir/bin/generic_json_fallback_10KB_bencher $generic_dir/json/client/fallback/small
-$GOEXEC build -v -o $output_dir/bin/generic_json_fallback_10KB_bencher $generic_dir/json/client/fallback/medium
+$GOEXEC build -v -o $output_dir/bin/generic_json_fallback_1KB_bencher $generic_dir/json/client/fallback/small
+$GOEXEC build -v -o $output_dir/bin/generic_json_fallback_5KB_bencher $generic_dir/json/client/fallback/medium
 $GOEXEC build -v -o $output_dir/bin/generic_json_fallback_10KB_bencher $generic_dir/json/client/fallback/large
 $GOEXEC build -v -o $output_dir/bin/generic_ordinary_1K_bencher $generic_dir/ordinary/client/small
 $GOEXEC build -v -o $output_dir/bin/generic_ordinary_5K_bencher $generic_dir/ordinary/client/medium
@@ -44,8 +44,8 @@ $GOEXEC build -v -o $output_dir/bin/generic_map_10KB_reciever $generic_dir/map
 $GOEXEC build -v -o $output_dir/bin/generic_json_default_1KB_reciever $generic_dir/json/server/default/small
 $GOEXEC build -v -o $output_dir/bin/generic_json_default_5KB_reciever $generic_dir/json/server/default/medium
 $GOEXEC build -v -o $output_dir/bin/generic_json_default_10KB_reciever $generic_dir/json/server/default/large
-$GOEXEC build -v -o $output_dir/bin/generic_json_fallback_10KB_reciever $generic_dir/json/server/fallback/small
-$GOEXEC build -v -o $output_dir/bin/generic_json_fallback_10KB_reciever $generic_dir/json/server/fallback/medium
+$GOEXEC build -v -o $output_dir/bin/generic_json_fallback_1KB_reciever $generic_dir/json/server/fallback/small
+$GOEXEC build -v -o $output_dir/bin/generic_json_fallback_5KB_reciever $generic_dir/json/server/fallback/medium
 $GOEXEC build -v -o $output_dir/bin/generic_json_fallback_10KB_reciever $generic_dir/json/server/fallback/large
 $GOEXEC build -v -o $output_dir/bin/generic_ordinary_1KB_reciever $generic_dir/ordinary
 $GOEXEC build -v -o $output_dir/bin/generic_ordinary_5KB_reciever $generic_dir/ordinary
