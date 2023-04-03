51937ff1cf6bb69860d6c7b6dd097ec808e67b89
Marina-Sakai <marina.sakai@bytedance.com>
2023-04-03 16:10:55 +0800

    change output format

--- scripts/benchmark_generic.sh
+++ scripts/benchmark_generic.sh
@@ -11,14 +11,23 @@ crepo=("generic_http_default" "generic_http_fallback" "generic_json_default" "ge
 ports=(8001 8001 8002 8003 8004 8005)
 data=("1KB" "5KB" "10KB")
 
+function finish_cmd_generic() {
+  # to csv report
+    ./scripts/reports/to_csv.sh output/"$1.log" > output/"$1".csv
+    echo "output reports: output/$1.log, output/$1.csv"
+    cat ./output/"$1.csv"
+}
+
 echo "Building generic services by exec build_generic.sh ..."
 source $CURDIR/build_generic.sh
 echo "Build finished."
 
 # benchmark
 for d in ${data[@]}; do
-  echo "------------------------------" | $tee_cmd
-  echo "Data size: $d" | $tee_cmd
+  report="$(date +%F-%H-%M)_${d}"
+  tee_cmd_generic="tee -a output/$report.log"
+  echo "------------------------------"
+  echo "Data size: $d"
   for b in ${body[@]}; do
     for c in ${concurrent[@]}; do
       for ((i = 0; i < ${#srepo[@]}; i++)); do
@@ -33,7 +42,7 @@ for d in ${data[@]}; do
 
         # run client
         echo "Client [$crp] running with [$cmd_client]"
-        $cmd_client $output_dir/bin/${crp}_${d}_bencher -addr="$addr" -b=$b -c=$c -n=$n --sleep=$sleep | $tee_cmd
+        $cmd_client $output_dir/bin/${crp}_${d}_bencher -addr="$addr" -b=$b -c=$c -n=$n --sleep=$sleep | $tee_cmd_generic
 
         # stop server
         pid=$(ps -ef | grep ${srp}_${d}_reciever | grep -v grep | awk '{print $2}')
@@ -44,6 +53,5 @@ for d in ${data[@]}; do
       done
     done
   done
+  finish_cmd_generic $report
 done
-
-finish_cmd
--- scripts/reports/to_csv.sh
+++ scripts/reports/to_csv.sh
@@ -2,15 +2,4 @@
 
 # csv
 # usage: to_csv.sh xxx.log
-# grep TPS "$1" | awk -F '[ :,]+' '{split($6,a,"m");split($8,b,"m");print $2","substr($11,3)","substr($9,4)","$4","a[1]","b[1]}'
-
-grep -E '^-{30}$|Data size:|TPS' "$1" | awk '
-/^-{30}$/ || /Data size:/ {
-    print $0
-}
-/TPS/ {
-    split($6,a,"m");
-    split($8,b,"m");
-    print $2","substr($11,3)","substr($9,4)","$4","a[1]","b[1]
-}
-'
+grep TPS "$1" | awk -F '[ :,]+' '{split($6,a,"m");split($8,b,"m");print $2","substr($11,3)","substr($9,4)","$4","a[1]","b[1]}'
